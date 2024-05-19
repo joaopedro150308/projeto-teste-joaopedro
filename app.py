3ae3b74ca4d317412f99d1cec2701e24ed298f60
João Pedro Souza Barbosa <joaopedro150308@gmail.com>
2024-05-19 20:39:00 -0300

    Versão 2

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print('Estamos no nosso primeiro projeto!')
+while True:
+    while True:
+        sei_la = str(input('Você quer começar à aprender a automatizar???? [S/N] ')).upper().strip()[0]
+        if sei_la in 'SN':
+            break
+    if sei_la == 'S':
+        print('Puxa! Eu também!!!')
+    else:
+        print('Problema seu! Eu quero.')
